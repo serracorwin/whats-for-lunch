4aac58660f782afeddcabf74ed7f5fd4fcfa2bf3
Corwin Kelly <corwin.rebma@gmail.com>
2016-01-27 13:55:46 -0500

    Add display of today's restaurant pick

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @restaurant = Restaurant.todays_pick
+    @restaurant.update(last_visit: Date.today) unless @restaurant.nil?
   end
 end
